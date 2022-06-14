10a1eaadbd7998081ebcd0f209650fe610df7d46
Valya <unsterdam@mail.ru>
2022-06-14 17:58:12 +0300

    making-def__str__

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -8,3 +8,7 @@ class News(models.Model): #шаг 9
     updated_at = models.DateTimeField(auto_now=True)#Время и дата обновления новости
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')#позволяет загружать изображения, upload_to позволяет выбирать куда именно загружать файл /%Y -год /%m -месяц позволяет поструктурно загружать фотки
     is_published = models.BooleanField(default=True)#новость по умолчанию публикуется 
+    
+
+    def __str__(self): #При вызове  News.objects.all() в shell консоли title будет выводится таким каким мы его задавали в переменной News1,News2 и т.д.
+        return self.title
