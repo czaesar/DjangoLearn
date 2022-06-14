1a36e80f11ec37d6df0188903502bba4a8533c02
Valya <unsterdam@mail.ru>
2022-06-14 17:24:52 +0300

    make-if-settings-DEBUG

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings 
+from django.conf.urls.static import static #возможно эту дичь вручную писать над будет
 from django.contrib import admin
 from django.urls import path, include #шаг 4 импорт include для того чтобы подключать другие urls
 
@@ -22,3 +25,7 @@ urlpatterns = [
     path('', include('news.urls')) #шаг 8, создаем пустую строку, чтобы показывало главную страницу без префиксов
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA__ROOT) #Формируем этот маршрут только в отладочном режиме
