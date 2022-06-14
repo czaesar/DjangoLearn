04898ab65217090694332bed491dd9f1e71c0cf6
Valya <unsterdam@mail.ru>
2022-06-14 17:15:16 +0300

    create-MEDIAD_ROOT-and-MEDIA_URL-in-settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')#в медиа будут загружаться все картинки
+MEDIA_URL = '/media/'#для загрузки файлов
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
