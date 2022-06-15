5bf8d4cf5e033aaf0145d86042482792acc72891
Valya <unsterdam@mail.ru>
2022-06-15 16:35:46 +0300

    make-index-and-render

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -3,13 +3,10 @@ from django.http import  HttpResponse
 from .models import News #импот созданных моделей
 
 
+
 def index(request):
     news = News.objects.all()
-    res = '<h1>Список новостей</h1>'
-    for item in news:
-        res+=f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
-    return HttpResponse(res)
-
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
 
 def test(request):
     return HttpResponse('Тестовая страница')
