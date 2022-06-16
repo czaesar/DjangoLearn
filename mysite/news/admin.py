bfcbbeffe7ec09999957a8d92ba9002731fd1f41
Valya <unsterdam@mail.ru>
2022-06-16 10:12:35 +0300

    some-change-in-admin

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -6,6 +6,8 @@ class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'category', 'title','created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'category')
 
 
 class CategoryAdmin(admin.ModelAdmin):
